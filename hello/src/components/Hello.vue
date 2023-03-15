204a89201b7dbff9963ffe31b25ca3577f6514b3
whq <2283015972@qq.com>
2023-03-15 13:38:56 +0800

    新增

---
+++ hello/src/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>hello</h1>
+</template>
+  
+  <script>
+  export default {
+    name: 'Hello',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+
+  <style>
+ 
+  </style>
+  
